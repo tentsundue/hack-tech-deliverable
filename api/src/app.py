4b55e242a087d30e57e3b3f5a7f360623d34418b
Tenzin Tsundue <97130706+tentsundue@users.noreply.github.com>
2024-04-16 21:23:00 -0700

    Initial commit

---
+++ api/src/app.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+from typing import TypedDict
+
+from fastapi import FastAPI, Form, status
+from fastapi.responses import RedirectResponse
+
+from services.database import JSONDatabase
+
+app = FastAPI()
+
+
+class Quote(TypedDict):
+    name: str
+    message: str
+    time: str
+
+
+database: JSONDatabase[list[Quote]] = JSONDatabase("data/database.json")
+
+
+@app.on_event("startup")
+def on_startup() -> None:
+    """Initialize database when starting API server."""
+    if "quotes" not in database:
+        print("Adding quotes entry to database")
+        database["quotes"] = []
+
+
+@app.on_event("shutdown")
+def on_shutdown() -> None:
+    """Close database when stopping API server."""
+    database.close()
+
+
+@app.post("/quote")
+def post_message(name: str = Form(), message: str = Form()) -> RedirectResponse:
+    """
+    Process a user submitting a new quote.
+    You should not modify this function except for the return value.
+    """
+    now = datetime.now().replace(microsecond=0)
+
+    quote = Quote(name=name, message=message, time=now.isoformat())
+    database["quotes"].append(quote)
+
+    # You may modify the return value as needed to support other functionality
+    return RedirectResponse("/", status.HTTP_303_SEE_OTHER)
+
+
+# TODO: add another API route with a query parameter to retrieve quotes based on max age
